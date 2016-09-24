bc9dfb16594565e26588fbacbb139b2b1f01805d
QaziPython <rafehqazi1@gmail.com>
2016-09-23 20:23:38 -0500

    Cut down project 2 from more than 20 lines to 1 line.

--- week_4/project_2/monte_carlo_business.py
+++ week_4/project_2/monte_carlo_business.py
@@ -4,47 +4,14 @@ Modified: September 2016
 Program: Calculate average hourly maximum customer volume.
 """
 
+import numpy
+from collections import Counter
 from random import randrange
-
-
-def count_common(my_list):
-    """
-    The number of times the most common element occurs in a list.
-    :param my_list: list
-    :return: int
-
-    >>> count_common([1, 1])
-    2
-    >>> count_common([1, 2, 2, 1, 2])
-    3
-    """
-    max_num = my_list.count(my_list[-1])
-    for element in my_list:
-        if my_list.count(element) > max_num:
-            max_num = my_list.count(element)
-    return max_num
 
 
 def ave_max_calc(C, H, D):
     """
     Simulates a business with customers, hours, and days.
     Outputs the average max hourly customer volume.
-    :param C: int
-    :param H: int
-    :param D: int
-    :return: float
     """
-    busy_max = []
-    for _ in range(D):
-        customers = []
-        for i in range(C):
-            customers.append(randrange(0, H))
-        busy_max.append(count_common(customers))
-
-    return sum(busy_max) / len(busy_max)
-
-
-if __name__ == '__main__':
-    import doctest
-
-    doctest.testmod()
+    return numpy.mean([Counter([randrange(0, H) for _ in range(C)]).most_common()[0][1] for _ in range(D)])
