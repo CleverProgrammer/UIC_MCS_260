18e1923ba8006563784d9c84cb836561e1367da9
QaziPython <rafehqazi1@gmail.com>
2016-09-23 19:44:09 -0500

    Complete Monte Carlo Business project 2 perfectly. Have it ready to submit.

--- week_4/project_2/monte_carlo_business.py
+++ week_4/project_2/monte_carlo_business.py
@@ -1,25 +1,44 @@
 from random import randrange
 
-def most_common(my_list):
-	max_num = my_list.count(my_list[-1])
-	for element in my_list:
-		my_list.count(element) > max_num
-		max_num = my_list.count(element)
-	return max_num
+
+def count_common(my_list):
+    """
+    The number of times the most common element occurs in a list.
+    :param my_list: list
+    :return: int
+
+    >>> count_common([1, 1])
+    2
+    >>> count_common([1, 2, 2, 1, 2])
+    3
+    """
+    max_num = my_list.count(my_list[-1])
+    for element in my_list:
+        if my_list.count(element) > max_num:
+            max_num = my_list.count(element)
+    return max_num
+
 
 def ave_max_calc(C, H, D):
-	busy_max = []
+    """
+    Simulates a business with customers, hours, and days.
+    Outputs the average max hourly customer volume.
+    :param C: int
+    :param H: int
+    :param D: int
+    :return: float
+    """
+    busy_max = []
+    for _ in range(D):
+        customers = []
+        for i in range(C):
+            customers.append(randrange(0, H))
+        busy_max.append(count_common(customers))
 
-	for i in range(D):
-		customers = []
+    return sum(busy_max) / len(busy_max)
 
 
-		for i in range(C):
-			print(customers)
-			customers.append(randrange(0, H))
+if __name__ == '__main__':
+    import doctest
 
-		busy_max.append(most_common(customers))
-
-	return sum(busy_max) // len(busy_max)
-
-print(ave_max_calc(50, 24, 100))
+    doctest.testmod()
