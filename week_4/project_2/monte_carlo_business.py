e3d12e25c7ca7dbdc1ec8ffaeb7be0b2ddb1955d
QaziPython <rafehqazi1@gmail.com>
2016-09-30 15:30:23 -0500

    Add ability for user to add input.

--- week_4/project_2/monte_carlo_business.py
+++ week_4/project_2/monte_carlo_business.py
@@ -15,3 +15,11 @@ def ave_max_calc(C, H, D):
     Outputs the average max hourly customer volume.
     """
     return numpy.mean([Counter([randrange(0, H) for _ in range(C)]).most_common()[0][1] for _ in range(D)])
+
+if __name__ == '__main__':
+    print("Hello, welcome to my average maximum hourly customer value calculator!")
+    C = int(input("Give me the number of customers per day: "))
+    H = int(input("Give me the number of business hours per day: "))
+    D = int(input("Give me the number of days to simulate: "))
+
+    print(ave_max_calc(C, H, D))
