9d02c01d78b2fa88e499f1a19a78ddb12b8a959d
Rafeh Qazi <qazi@cleverprogrammer.com>
2018-02-18 13:20:45 -0800

    Improve documentation

--- week_4/lab_2/scrabble.py
+++ week_4/lab_2/scrabble.py
@@ -6,14 +6,16 @@ Program: Scrabble
 
 Scrabble Points Per Letter:
 ---------------------------------------------------------------------
-1 point: E ×12, A ×9, I ×9, O ×8, N ×6, R ×6, T ×6, L ×4, S ×4, U ×4
-2 points: D ×4, G ×3
-3 points: B ×2, C ×2, M ×2, P ×2
-4 points: F ×2, H ×2, V ×2, W ×2, Y ×2
-5 points: K ×1
-8 points: J ×1, X ×1
-10 points: Q ×1, Z ×1
+1 point: E, A, I, O, N, R, T, L, S, U
+2 points: D, G
+3 points: B, C, M, P
+4 points: F, H, V, W, Y
+5 points: K
+8 points: J, X
+10 points: Q, Z
 ---------------------------------------------------------------------
+
+50 Point Bonus: If the length of a word is 7 letters or more.
 """
 SCRABBLE_POINTS = {
     'eaionrtlsu': 1,
